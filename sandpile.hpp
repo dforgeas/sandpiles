e8a3c6722fe7b5c278654e18df581881d4e259f0
David Forgeas <david.forgeas@gmail.com>
2020-06-21 15:20:49 +0100

    Try to read an identity file instead of computing it again

--- sandpile.hpp
+++ sandpile.hpp
@@ -90,8 +90,56 @@ private:
 	}
 };
 
+template <int N>
+Sandpile<N> readPGM(const char*const filename) {
+	using namespace std::string_literals;
+	std::ifstream in(filename, std::ios::binary);
+	if (not in) {
+		throw std::runtime_error(""); // expected case, no need for an error message
+	}
+	std::string header;
+	if (not (std::getline(in, header) and header == "P5 " + std::to_string(N) + ' ' + std::to_string(N) + " 3")) {
+		throw std::runtime_error("invalid sandpile header in "s + filename);
+	}
+	Sandpile<N> result;
+	result.a.resize(N * N);
+	in.read(reinterpret_cast<char*>(&result.a[0]), result.a.size());
+	if (in.gcount() != result.a.size()) {
+		throw std::runtime_error("invalid sandpile data in "s + filename);
+	}
+	return result;
+}
+
+inline std::string makeFilename(const int N) {
+	using namespace std::string_literals;
+	return "sandpile"s + std::to_string(N) + ".pgm";
+}
+
+template <int N>
+bool tryToReadIdentity(std::ostream &out) {
+	const auto filename(makeFilename(N));
+	try {
+		const auto identityPile(readPGM<N>(filename.c_str()));
+#if 0 // would make sense but it seemingly takes as much time as writeIdentity
+		const auto pile(Sandpile<N>::makeMax());
+		const auto newPile(pile + identityPile);
+		const bool ok(newPile == pile);
+		if (not ok) {
+			out << "Read " << filename << "but it isn't the indentity pile for size " << N << '.' << std::endl;
+		}
+		return ok;
+#endif
+		return true;
+	} catch (std::exception const& exc) {
+		if (*exc.what()) { // if it doesn't start with a nul terminator
+			out << exc.what() << std::endl;
+		}
+		return false;
+	}
+}
+
 template<int N>
-void writePGM(const Sandpile<N> &s, const char*filename) {
+void writePGM(const Sandpile<N> &s, const char*const filename) {
 	std::ofstream out(filename, std::ios::binary);
 	out << "P5 " << N << ' ' << N << " 3\n";
 	out.write(reinterpret_cast<const char*>(&s.a[0]), s.a.size());
@@ -99,9 +147,8 @@ void writePGM(const Sandpile<N> &s, const char*filename) {
 
 template<int N>
 void writeIdentity() {
-	using namespace std::string_literals;
 	const auto pile(Sandpile<N>::makeIdentity());
-	const auto filename("sandpile"s + std::to_string(N) + ".pgm");
+	const auto filename(makeFilename(N));
 	writePGM(pile, filename.c_str());
 }
 
@@ -128,7 +175,14 @@ inline void decoFacP(std::ostream &out, int n) {
 template<int N>
 void makeIdentity(std::ostream &out) {
 	decoFacP(out, N);
-	writeIdentity<N>();
+	if (not tryToReadIdentity<N>(out)) {
+		writeIdentity<N>();
+	}
+}
+
+template<>
+void makeIdentity<0>(std::ostream &out) {
+	// do nothing
 }
 
 template <int...Ns>
